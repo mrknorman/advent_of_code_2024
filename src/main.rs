daf3b5d96241bca75b3e9aace136b01e6b1e2f33
Michael Norman <michaelnorman@CSTs-MacBook-Pro.local>
2024-12-03 17:36:51 +0000

    Day 3

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
 mod one;
+mod two;
+mod three;
 
 fn main() {
     one::one_a();
     one::one_b();
+
+    // 6.7 ms ± 4.2 ms 
+    two::two_a();
+
+    // 6.4 ms ± 3.9 ms
+    two::two_b();
+
+    three::three_a();
+    three::three_b();
 }
